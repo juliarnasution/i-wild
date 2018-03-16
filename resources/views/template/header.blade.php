613cc11053476df0e18a5504bfc0f92cbfa24cbf
Juliar Nasution <juliarnasution@std.unissula.ac.id>
2018-03-16 09:03:49 +0700

    menambahkan file css bootstrap

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css')!!}">
+        <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css') !!}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
